ad4955b6708878fbe1aa609f0344f853b2a20bdf
Hamzakhann <hamzakhann66@gmail.com>
2021-12-18 12:36:09 +0500

    login and sign up Ui complete

---
+++ src/Screens/public-sign-up/sign.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { View , Text , StyleSheet ,TextInput , TouchableOpacity} from 'react-native';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+
+const SignUpPublic = () => {
+    let [fontsLoaded] = useFonts({
+        Inter_900Black,
+      });
+    let [email , setEmail] = useState('');
+    let [password , setPassword] = useState('');
+    let [age , setAge] = useState('');
+    let [name , setName] = useState('');
+    function signUpFunc(){
+        console.log({email , password})
+    }
+    return (
+        <View style={styles.container}>
+            <View style={styles.view1}>
+                <TextInput placeholder="Email" style={styles.input} value={email} onChangeText={(e)=>{setEmail(e)}}  />
+            </View>
+            <View style={styles.view2}>
+                <TextInput placeholder="name" style={styles.input} value={name} onChangeText={(e)=>{setName(e)}}  />
+            </View>
+            <View style={styles.view3}>
+                <TextInput placeholder="age" style={styles.input} value={age} onChangeText={(e)=>{setAge(e)}}  />
+            </View>
+            <View style={styles.view4}>
+                <TextInput placeholder="Password" style={styles.input} value={password} onChangeText={(e)=>{setPassword(e)}}  />
+            </View>
+            <View style={styles.btnDiv}>
+                <TouchableOpacity style={styles.touchableBtn} onPress={signUpFunc}><Text style={{color:"white"}}>Sign in</Text></TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    view1 : {
+        paddingTop : 10,
+        backgroundColor : 'white',
+        paddingLeft: 10,
+        paddingRight:10,
+        borderTopEndRadius:10,
+        paddingBottom:10,
+        borderTopStartRadius:10,
+    },
+    view2 : {
+        paddingTop : 10,
+        backgroundColor : 'white',
+        paddingLeft: 10,
+        paddingRight:10,
+        paddingBottom:10,
+    },
+    view3 : {
+        paddingTop : 10,
+        backgroundColor : 'white',
+        paddingLeft: 10,
+        paddingRight:10,
+        paddingBottom:10,
+    },
+    view4 : {
+        // paddingTop : 5,
+        backgroundColor : 'white',
+        paddingLeft: 10,
+        paddingRight:10,
+        paddingBottom:20,
+    },
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      borderColor : 'rgb(195, 195, 195)',
+      padding: 10,
+      borderRadius : '5px',
+
+    //   backgroundColor : '#ffffff'
+    },
+    container :{
+        flex:1,
+        justifyContent : "center",
+        alignItems : 'center',
+        fontFamily : 'Inter_400Black',
+        width : '100%',
+        height : '100%',
+        backgroundColor: "#dedfe0", 
+    },
+    btnDiv : {
+        // marginTop : 10,
+        paddingTop : 10,
+        paddingLeft: 19,
+        paddingRight : 19,
+        paddingBottom : 10,
+        backgroundColor : 'white',
+        borderBottomEndRadius: 10,
+        borderBottomStartRadius :10,
+    },
+    touchableBtn: {
+        // height: '3rem',
+        // width : '10rem'
+        paddingTop: 10,
+        paddingBottom : 10,
+        paddingLeft : 90,
+        backgroundColor : 'black',
+
+        paddingRight: 90,
+    }
+  });
+
+export default SignUpPublic;
--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -2,17 +2,9 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../Screens/home';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import ShowResult from '../Screens/show-result';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import MainLogin from '../Screens/login/login';
+import SignUpPublic from '../Screens/public-sign-up/sign';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -24,8 +16,8 @@ function App() {
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator> */}
         <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={Home} />
-            <Drawer.Screen name="Show Result" component={ShowResult} />
+            <Drawer.Screen name="Login" component={MainLogin} />
+            <Drawer.Screen name="Sign-up" component={SignUpPublic} />
         </Drawer.Navigator>
     </NavigationContainer>
   );
