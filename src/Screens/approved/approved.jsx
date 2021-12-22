769f2ffe74c66e087e3014910841127136ff53de
AbdulSaboor100 <abdulsaboor5808@gmail.com>
2021-12-22 12:53:49 +0500

    All Css Styling Is DOne

--- src/Screens/approved/approved.jsx
+++ src/Screens/approved/approved.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import { State } from 'react-native-gesture-handler';
 import { GlobalContext } from '../../context/context';
 
@@ -15,47 +15,47 @@ const Approved = () => {
             {
                 state.approvedApplication ? (
                     state.approvedApplication.status === "approved" ? (
-                        <View>
-                            <View>
-                                <Text>Khana Sab Ke Liye</Text>
+                        <View style={styles.container}>
+                            <View style={styles.status}>
+                                <Text style={styles.special}>Khana Sab Ke Liye</Text>
                             </View>
-                            <View>
-                                <Text>Your Application Status : {state.approvedApplication.status}</Text>
+                            <View style={styles.status1}>
+                                <Text style={styles.text}>Your Application Status : {state.approvedApplication.status}</Text>
                             </View>
-                            <View>
-                                <Text>Father Name : {state.approvedApplication.approvedObj.fatherName}</Text>
+                            <View style={styles.status}>
+                                <Text style={styles.text}>Father Name : {state.approvedApplication.approvedObj.fatherName}</Text>
                             </View>
-                            <View>
-                                <Text>Cnic No : {state.approvedApplication.approvedObj.cnic}</Text>
+                            <View style={styles.status}>
+                                <Text style={styles.text}>Cnic No : {state.approvedApplication.approvedObj.cnic}</Text>
                             </View>
-                            <View>
-                                <Text>Contact No : 92-311-1729526</Text>
+                            <View style={styles.status}>
+                                <Text style={styles.text}>Contact No : 92-311-1729526</Text>
                             </View>
-                            <View>
-                                <Text>Food Bank Branch Name</Text>
-                                <Text>{state.approvedApplication.approvedObj.nearestOne.name}</Text>
+                            <View style={styles.status}>
+                                <Text style={styles.text}>Food Bank Branch Name : </Text>
+                                <Text style={styles.special2}>{state.approvedApplication.approvedObj.nearestOne.name}</Text>
                             </View>
                         </View>
                     ) : (
-                        <View>
-                            <View>
-                                <Text>Khana Sab Ke Liye</Text>
+                        <View style={styles.container}>
+                            <View  style={styles.status}>
+                                <Text style={styles.special}>Khana Sab Ke Liye</Text>
                             </View>
-                            <View>
-                                <Text>Your Application Status : {state.approvedApplication.status}</Text>
+                            <View style={styles.status}>
+                                <Text style={styles.text}>Your Application Status : {state.approvedApplication.status}</Text>
                             </View>
-                            <View>
-                                <Text>Father Name : {state.approvedApplication.rejectObj.fatherName}</Text>
+                            <View style={styles.status}>
+                                <Text style={styles.text}>Father Name : {state.approvedApplication.rejectObj.fatherName}</Text>
                             </View>
-                            <View>
-                                <Text>Cnic No : {state.approvedApplication.rejectObj.cnic}</Text>
+                            <View style={styles.status}>
+                                <Text style={styles.text}>Cnic No : {state.approvedApplication.rejectObj.cnic}</Text>
                             </View>
-                            <View>
-                                <Text>Contact No : 92-311-1729526</Text>
+                            <View style={styles.status}> 
+                                <Text style={styles.text}>Contact No : 92-311-1729526</Text>
                             </View>
-                            <View>
-                                <Text>Food Bank Branch Name</Text>
-                                <Text>{state.approvedApplication.rejectObj.nearestOne.name}</Text>
+                            <View style={styles.status}>
+                                <Text style={styles.text}>Food Bank Branch Name : </Text>
+                                <Text style={styles.special2}>{state.approvedApplication.rejectObj.nearestOne.name}</Text>
                             </View>
                         </View>
                     )
@@ -69,6 +69,41 @@ const Approved = () => {
 
     )
 }
+
+
+const styles = StyleSheet.create({
+    container : {
+        width:"100%",
+        height:"100%",
+        flex:1,
+        justifyContent : 'center',
+        alignItems:'flex-start',
+        margin: 60,
+
+    },
+    text:{
+        fontSize:15,
+        fontWeight:'bold',
+        color:'#808080',
+        
+    },
+    special : {
+        fontSize:30,
+        // color:'#89c343',
+        color:'#808080',
+        fontWeight:'100',
+        marginTop:20
+    },
+    status1 : {
+        marginTop:20
+    },
+    special2:{
+        marginTop:20,
+        color:'#808080',
+        fontWeight:'500',
+        fontSize:25,
+    },
+})
 
 export default Approved;
 {/* <View style={{flex : 1 , justifyContent:'center' , paddingTop : 200 , alignItems : 'center' ,width:'100%' , height :'100%'}}>
